01a080cbf73dd60de3c8a6eae4fa8b61e28de010
DannerDQ <diazquispedanner@gmail.com>
2024-12-14 12:46:43 -0500

    feat: add header & search bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
+import SearchBar from "@/components/SearchBar";
 import { props } from "@/types";
 import "./globals.css";
 
 export default function RootLayout({ children }: props) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <header>
+          <h1>Gif Hub</h1>
+          <div>
+            <SearchBar />
+          </div>
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,8 @@
+export default function SearchBar() {
+  return (
+    <form>
+      <input type="search" name="query" id="query" />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+}
